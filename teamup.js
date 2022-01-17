f16c34da9127ba9893fa695a2f99f9088cb2f2e6
Adam Edison <adam.edison@gmail.com>
2022-01-16 19:21:00 -0500

    Update teamup.js

--- teamup.js
+++ teamup.js
@@ -1,11 +1,33 @@
 // When the "Today" button is clicked,
 // and that button is disabled (not changing from a different day),
 // Scroll to the current time in center of view
-$('.toolbar-button.today.disabled').click(function() {
-  
-  $(".time-indicator-line")[0].scrollIntoView({
-    behavior: "instant",
-    block: "center"
-  });
 
+const addScrollToNowBehavior = (element) => {
+	
+	element.addEventListener("click", () => {
+		document.querySelector(".time-indicator-line").scrollIntoView({
+	    	behavior: "instant",
+	    	block: "center"
+		});
+	});
+}
+
+// NOTE: The observer is required to make sure the element is loaded
+// before attempting to attach an event listener to that element
+const observer = new MutationObserver((mutations, obs) => {
+	
+	const todayButton = document.querySelector(".toolbar-button.today");
+  
+	if (todayButton) {
+	    addScrollToNowBehavior(todayButton);
+	}
+	
+	obs.disconnect();
+});
+
+
+// executes immediately
+observer.observe(document, {
+  childList: true,
+  subtree: true
 });
