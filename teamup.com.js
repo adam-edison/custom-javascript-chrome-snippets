77830d7ed87cc01ba01de024900324ca6a74d666
Adam Edison <adam.edison@kasa.com>
2022-01-16 20:00:42 -0500

    Add prettier configuration

--- teamup.com.js
+++ teamup.com.js
@@ -3,32 +3,28 @@
 // Scroll to the current time in center of view
 
 const addScrollToNowBehavior = (element) => {
-	
-	element.addEventListener("click", () => {
-		document.querySelector(".time-indicator-line").scrollIntoView({
-	    	behavior: "instant",
-	    	block: "center"
-		});
-	});
-
+    element.addEventListener('click', () => {
+        document.querySelector('.time-indicator-line').scrollIntoView({
+            behavior: 'instant',
+            block: 'center',
+        })
+    })
 }
 
 // NOTE: The observer is required to make sure the element is loaded
 // before attempting to attach an event listener to that element
 const observer = new MutationObserver((mutations, obs) => {
-	
-	const todayButton = document.querySelector(".toolbar-button.today");
-  
-	if (todayButton) {
-	    addScrollToNowBehavior(todayButton);
-	}
-	
-	obs.disconnect();
-});
+    const todayButton = document.querySelector('.toolbar-button.today')
 
+    if (todayButton) {
+        addScrollToNowBehavior(todayButton)
+    }
+
+    obs.disconnect()
+})
 
 // executes immediately
 observer.observe(document, {
-  childList: true,
-  subtree: true
-});
+    childList: true,
+    subtree: true,
+})
